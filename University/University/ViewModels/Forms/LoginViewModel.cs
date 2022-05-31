dd31de4c193e2b94d582298c30dea68a8e4e7869
Christian Hernandez <cristian_estiven@hotmail.com>
2022-05-31 00:50:29 -0500

    2

--- University/University/ViewModels/Forms/LoginViewModel.cs
+++ University/University/ViewModels/Forms/LoginViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 
 namespace University.ViewModels.Forms
@@ -12,8 +14,29 @@ namespace University.ViewModels.Forms
         async void Login()
 
         {
+            var data = new { email = _email, password = _password };
+            var json = JsonConvert.SerializeObject(data);
+            var req  = new StringContent(json, Encoding.UTF8, "application/json");
+            var url = "https://reqres.in/api/login";
+            var result = string.Empty;
+            
+            using (var client = new HttpClient())
 
+            {
+                var response = await client.PostAsync(url, req);
 
+                if(response.IsSuccessStatusCode)
+
+                {
+
+                    //TODO: Logic APP
+                }
+
+                var statusCode = response.StatusCode;
+                result = await response.Content.ReadAsStringAsync();
+            }
+
+             
         }
 
     }
