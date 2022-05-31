8821a125527e1f444faa1b3baae994ae320a0a64
Christian Hernandez <cristian_estiven@hotmail.com>
2022-05-31 00:22:40 -0500

    1

---
+++ University/University/ViewModels/Forms/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace University.ViewModels.Forms
+{
+    public class LoginViewModel
+    {
+        private string _email;
+        private string _password;
+
+        async void Login()
+
+        {
+
+
+        }
+
+    }
+}
