2fc3bdb2466f25e82a8cf25e6761a41080f683ab
Christian Hernandez <cristian_estiven@hotmail.com>
2022-05-31 20:00:23 -0500

    31 mayo 8 pm

---
+++ University/University/DTOs/LoginDTO.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace University.DTOs
+{
+    public class LoginReqDTO
+    {
+        [JsonProperty("email")]
+        public string email { get; set; }
+
+        [JsonProperty("password")]
+        public string password { get; set; }
+
+    }
+
+    public class LoginResDTO
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+    }
+
+    public class LoginResFailDTO
+    {
+         [JsonProperty("error")]
+        public string Error { get; set; 
+
+    }
+}
