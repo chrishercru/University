48da628d4704af7756c6f17dce91d99a67d39872
Christian Hernandez <cristian_estiven@hotmail.com>
2022-05-31 19:38:55 -0500

    31 mayo 7:38pm

--- University/University/App.xaml.cs
+++ University/University/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using University.Views.Forms;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace University
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
--- University/University/ViewModels/Forms/LoginViewModel.cs
+++ University/University/ViewModels/Forms/LoginViewModel.cs
@@ -3,19 +3,38 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
-using static System.Net.Mime.MediaTypeNames;
+using Xamarin.Forms;
 
 namespace University.ViewModels.Forms
 {
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
+        #region Attributos
         private string _email;
         private string _password;
+        #endregion
 
+
+        #region propertis
+        public string Email
+        {
+
+            get { return _email; }
+            set { this.SetValue(ref _email, value); }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { this.SetValue(ref _password, value); }
+        }
+        #endregion
+
+        #region methods
         async void Login()
 
         {
-            var data = new { email = _email, password = _password };
+            var data = new { email = this.Email, password = this.Password };
             var json = JsonConvert.SerializeObject(data);
             var req  = new StringContent(json, Encoding.UTF8, "application/json");
             var url = "https://reqres.in/api/login";
@@ -27,17 +46,23 @@ namespace University.ViewModels.Forms
                 var response = await client.PostAsync(url, req);
 
                 if(response.IsSuccessStatusCode)
-
-                {       //TODO: logic APP
-                    await Application.Current.MainPage.DisplayAlert("Notify", "Login OK", "Cancel");
+                //TODO: logic APP
+                {
+                  await Application.Current.MainPage.DisplayAlert("Notify", "Login OK", "Cancel");
                 }
+
+
+                #region comands
+
 
                 var statusCode = response.StatusCode;
                 result = await response.Content.ReadAsStringAsync();
+
+                #endregion}
             }
 
-             
-        }
 
+        }
+        #endregion
     }
 }
