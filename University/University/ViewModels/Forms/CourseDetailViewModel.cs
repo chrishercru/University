c614d9d92013de5ed5e96e0ef65114fe413cdb5c
Christian Hernandez <cristian_estiven@hotmail.com>
2022-06-07 20:01:07 -0500

    7 Junio 2022

---
+++ University/University/ViewModels/Forms/CourseDetailViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using University.DTOs;
+
+namespace University.ViewModels.Forms
+{
+    public class CourseDetailViewModel : BaseViewModel
+    {
+        private CourseDTO _course;
+
+        public CourseDTO Course
+
+        {
+            get { return _course; }
+            set { this.SetValue(ref _course, value); }
+        }
+
+        public CourseDetailViewModel(CourseDTO course)
+        {
+            this.Course = course;
+        }
+        public CourseDetailViewModel()
+        {
+
+        }
+    }
+}
--- University/University/ViewModels/Forms/CourseItemViewModel.cs
+++ University/University/ViewModels/Forms/CourseItemViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using University.DTOs;
+using University.Views.Forms;
 using Xamarin.Forms;
 
 namespace University.ViewModels.Forms
@@ -11,6 +12,9 @@ namespace University.ViewModels.Forms
         async void OnItemClick()
         {
             await Application.Current.MainPage.DisplayAlert("Notify", $"Selected {this.Title}", "Cancel");
+            CourseDetailPage detailPage = new CourseDetailPage();
+            detailPage.BindingContext = new CourseDetailViewModel(this);
+            await Application.Current.MainPage.Navigation.PushAsync(detailPage);
         }
         public Command OnItemClickCommand { get; set; }
 
